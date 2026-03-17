0cd186b974744ad6f13398d48cb291208a848fa0
Jens Langhammer <jens.langhammer@beryju.org>
2026-03-17 13:00:10 +0100

    push to ghcr

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,3 +1,13 @@
 import config from "@goauthentik/prettier-config";
 
-export default config;
+export default {
+    ...config,
+    overrides: [
+        {
+            files: ["*.yml", "*.yaml"],
+            options: {
+                tabWidth: 2,
+            },
+        },
+    ],
+};
